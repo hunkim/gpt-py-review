1bc9911414f5ffbce068f81f862ac7bed633f82d
Sung Kim <hunkim@gmail.com>
2023-02-05 13:41:08 +0900

    Fixed entry point

--- gpt_py_review/extract.py
+++ gpt_py_review/extract.py
@@ -1,5 +1,6 @@
 import ast
 import os
+import sys
 import openai
 from ast import FunctionDef, ClassDef, AsyncFunctionDef
 
@@ -19,6 +20,7 @@ Think of corner cases and get creative and write complete pytest test code!
 Put text expnation in comment with #\n\n\n
 """
 
+
 def get_token_count(*strings):
     return sum([len(string) for string in strings])*2
 
@@ -28,7 +30,7 @@ def get_gpt3_response(text, prompt=REVIEW_PROMPT):
         results = openai.Completion.create(
             engine=GPT_ENGINE,
             prompt=prompt + "\n" + text,
-            n = 1,
+            n=1,
             max_tokens=4000 - get_token_count(prompt) - get_token_count(text),
         )
         # https://sharegpt.com/c/fZUq6nU
@@ -41,7 +43,8 @@ def get_gpt3_response(text, prompt=REVIEW_PROMPT):
         return "GPT3 Return Error: " + str(e)
     except Exception as e:
         return "GPT3 Error: " + str(e)
-    
+
+
 def open_file(filename, suffix=".comments", ext=None):
     base, ext_org = os.path.splitext(filename)
     if ext is None:
@@ -56,12 +59,13 @@ def open_file(filename, suffix=".comments", ext=None):
         outfile = f"{base}{suffix}_{i}{ext}"
 
     return open(outfile, "w")
-        
+
+
 def get_gpt(lines, prompt=REVIEW_PROMPT):
     code = "".join(lines)
-    return gu.get_gpt3_response(code, prompt=prompt)
-    
-    
+    return get_gpt3_response(code, prompt=prompt)
+
+
 def extract_functions(code):
     functions = {}
     tree = None
@@ -69,17 +73,27 @@ def extract_functions(code):
         tree = ast.parse(code)
     except SyntaxError as e:
         return f"Error: Could not parse code ({e})"
-    
+
     for node in ast.walk(tree):
         if isinstance(node, (FunctionDef, ClassDef, AsyncFunctionDef)):
-            functions[node.name] = code.split('\n')[node.lineno-1:node.body[-1].lineno]
+            functions[node.name] = code.split(
+                '\n')[node.lineno-1:node.body[-1].lineno]
 
     return functions
 
-def main(filename ):
+
+def main():
+
+    if len(sys.argv) < 2:
+        print("Usage: python script.py filename")
+        return
+
+    filename = sys.argv[1]
+    # rest of your code
+
     with open(filename, "r") as f:
         code = f.read()
-        
+
     review_file = open_file(filename, suffix=".review", ext=".md")
     test_file = open_file(filename, suffix=".test", ext=".py")
 
@@ -89,12 +103,9 @@ def main(filename ):
         function_body = "\n".join(functions[function_name])
         gpt_comment = get_gpt(function_body, prompt=REVIEW_PROMPT)
         review_file.writelines(gpt_comment)
-        
+
         gpt_test = get_gpt(function_body, prompt=TESTCASE_PROMPT)
         test_file.writelines(gpt_test)
-        
+
     review_file.close()
     test_file.close()
-    
-if __name__ == "__main__":
-    main("gpt_py_review/gpt_py_review/extract.py")
